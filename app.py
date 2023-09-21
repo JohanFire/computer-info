ccb9827cadb6bf8f7ac7298d28208ac778b2327e
Tristan Garcia Johan Desailly (BGSW/EBS4.3-SX) <JohanDesailly.TristanGarcia@mx.bosch.com>
2023-09-21 15:51:32 -0600

    cleaning code

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import os
 import psutil
-import uuid
-import re
 
-from utils.mac import *
-
+from utils import mac
 
 print('\n==============================================')
+print('\tComputer information')
+print('==============================================')
 print(f"Physical Cores: {psutil.cpu_count(logical=False)}")
 print(f'Logical processors (CPU cores count): {os.cpu_count()}')
 print('-----')
 print(f'RAM memory used: {psutil.virtual_memory()[2]}% ')
 print(f'RAM used (GB): {psutil.virtual_memory()[3]/1000000000}')
 print('-----')
-get_mac()
-get_mac_devices()
+mac.get_mac()
 print('==============================================\n')

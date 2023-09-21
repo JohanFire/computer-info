db16aa3978d89c5494dd91874dc5c07a47ef2b75
Tristan Garcia Johan Desailly (BGSW/EBS4.3-SX) <JohanDesailly.TristanGarcia@mx.bosch.com>
2023-09-21 15:41:52 -0600

    getting MAC address altough not formated yet

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import psutil
+import uuid
 
 print('\n==============================================')
 print(f"Physical Cores: {psutil.cpu_count(logical=False)}")
@@ -7,4 +8,8 @@ print(f'Logical processors (CPU cores count): {os.cpu_count()}')
 print('-----')
 print(f'RAM memory used: {psutil.virtual_memory()[2]}% ')
 print(f'RAM used (GB): {psutil.virtual_memory()[3]/1000000000}')
+print('-----')
+# printing the value of unique MAC
+# address using uuid and getnode() function
+print (hex(uuid.getnode()))
 print('==============================================\n')

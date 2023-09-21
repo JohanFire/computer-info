816cd1032c9ed5cf16eb2c1aa66ad3e914c4c966
Tristan Garcia Johan Desailly (BGSW/EBS4.3-SX) <JohanDesailly.TristanGarcia@mx.bosch.com>
2023-09-21 15:45:13 -0600

    get MAC address formatted

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import os
 import psutil
 import uuid
+import re
+
+def get_mac():
+    # joins elements of getnode() after each 2 digits.
+    print ("MAC address: ", end="")
+    print (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)
+    for ele in range(0,8*6,8)][::-1]))
+
+def get_mac_easier():
+    # joins elements of getnode() after each 2 digits.
+    # using regex expression
+    print ("The MAC address in formatted and less complex way is : ", end="")
+    print (':'.join(re.findall('..', '%012x' % uuid.getnode())))
 
 print('\n==============================================')
 print(f"Physical Cores: {psutil.cpu_count(logical=False)}")
@@ -9,7 +22,5 @@ print('-----')
 print(f'RAM memory used: {psutil.virtual_memory()[2]}% ')
 print(f'RAM used (GB): {psutil.virtual_memory()[3]/1000000000}')
 print('-----')
-# printing the value of unique MAC
-# address using uuid and getnode() function
-print (hex(uuid.getnode()))
+get_mac()
 print('==============================================\n')

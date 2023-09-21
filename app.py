d1d5960865d81ef551c2431552a704f8b26f73f5
Tristan Garcia Johan Desailly (BGSW/EBS4.3-SX) <JohanDesailly.TristanGarcia@mx.bosch.com>
2023-09-21 15:47:27 -0600

    get the MAC addresses of all the network interfaces on the computer

--- app.py
+++ app.py
@@ -14,6 +14,16 @@ def get_mac_easier():
     # using regex expression
     print ("The MAC address in formatted and less complex way is : ", end="")
     print (':'.join(re.findall('..', '%012x' % uuid.getnode())))
+    
+def get_mac_devices():
+    """ get the MAC addresses of all the network interfaces on the computer """
+    # Iterate over all the keys in the dictionary
+    for interface in psutil.net_if_addrs():
+        # Check if the interface has a valid MAC address
+        if psutil.net_if_addrs()[interface][0].address:
+            # Print the MAC address for the interface
+            print(psutil.net_if_addrs()[interface][0].address)
+            break
 
 print('\n==============================================')
 print(f"Physical Cores: {psutil.cpu_count(logical=False)}")
@@ -23,4 +33,5 @@ print(f'RAM memory used: {psutil.virtual_memory()[2]}% ')
 print(f'RAM used (GB): {psutil.virtual_memory()[3]/1000000000}')
 print('-----')
 get_mac()
+get_mac_devices()
 print('==============================================\n')

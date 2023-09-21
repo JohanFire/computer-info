ba5d13f192ce4c1ba2c999885ec5d89d77a24277
Tristan Garcia Johan Desailly (BGSW/EBS4.3-SX) <JohanDesailly.TristanGarcia@mx.bosch.com>
2023-09-21 16:27:18 -0600

    cleaning

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ import psutil
 
 from utils import mac, ip
 
-print('\n==============================================')
-print('\tComputer information')
-print('==============================================')
+print('\n' + '=' * 50)
+print('\t\tComputer information')
+print('=' * 50)
+print('=' * 20 + ' Main Info ' + '=' * 19)
+ip.get_ipv4_address()
+mac.get_mac()
+print('-----')
 print(f"Physical Cores: {psutil.cpu_count(logical=False)}")
 print(f'Logical processors (CPU cores count): {os.cpu_count()}')
 print('-----')
 print(f'RAM memory used: {psutil.virtual_memory()[2]}% ')
 print(f'RAM used (GB): {psutil.virtual_memory()[3]/1000000000}')
 print('-----')
-ip.get_ipv4_address()
-mac.get_mac()
+
 print('==============================================\n')

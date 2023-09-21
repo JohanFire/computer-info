73f1f461e6ebf62baa2d97237e102cd46f636a51
Tristan Garcia Johan Desailly (BGSW/EBS4.3-SX) <JohanDesailly.TristanGarcia@mx.bosch.com>
2023-09-21 15:38:46 -0600

    Initial Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+import psutil
+
+print('\n==============================================')
+print(f"Physical Cores: {psutil.cpu_count(logical=False)}")
+print(f'Logical processors (CPU cores count): {os.cpu_count()}')
+print('-----')
+print(f'RAM memory used: {psutil.virtual_memory()[2]}% ')
+print(f'RAM used (GB): {psutil.virtual_memory()[3]/1000000000}')
+print('==============================================\n')

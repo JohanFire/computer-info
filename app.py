f1ba11479888d7d1f9108616d203b32117cf9132
Tristan Garcia Johan Desailly (BGSW/EBS4.3-SX) <JohanDesailly.TristanGarcia@mx.bosch.com>
2023-09-22 11:37:58 -0600

    memory

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import psutil
 
-from utils import cpu, ip, mac
+from utils import cpu, ip, mac, memory
 
 def main():
     try:
@@ -15,17 +15,21 @@ def main():
     print('\n' + '=' * 20 + ' Main Info ' + '=' * 19)
     ip.get_ipv4_address()
     mac.get_mac()
-    print('-----')
+    print('-' * 20)
     print(f"Physical Cores: {psutil.cpu_count(logical=False)}")
     print(f'Logical processors (total CPU cores): {os.cpu_count()}')
-    print('-----')
+    print('-' * 20)
     print(f'RAM memory used: {psutil.virtual_memory()[2]}% ')
     print(f'RAM used (GB): {psutil.virtual_memory()[3]/1000000000}')
-    print()
-    print('=' * 20 + ' CPU Info ' + '=' * 20)
+    print('\n' + '=' * 20 + ' CPU Info ' + '=' * 20)
     cpu.cpu_cores()
     cpu.cpu_frequencies()
     cpu.cpu_usage()
+    print('\n' + '=' * 20 + ' Memory Info ' + '=' * 17)
+    memory.memory_info()
+    print('-'*7 + ' SWAP ' + '-'*7)
+    memory.swap_info()
+
 
 if __name__ == '__main__':
     main()
--- utils/memory.py
+++ utils/memory.py
@@ -13,20 +13,32 @@ def get_size(bytes, suffix="B"):
             return f"{bytes:.2f}{unit}{suffix}"
         bytes /= factor
 
-def main():
+def memory_info():
     # get the memory details
     svmem = psutil.virtual_memory()
     print(f"Total: {get_size(svmem.total)}")
     print(f"Available: {get_size(svmem.available)}")
     print(f"Used: {get_size(svmem.used)}")
-    print(f"Percentage: {svmem.percent}%")
-    print("="*20, "SWAP", "="*20)
+    print(f"Used percentage: {svmem.percent}%")
+    
+    result = [get_size(svmem.total), get_size(svmem.available), get_size(svmem.used), f'{svmem.percent}%']
+
+    return 
+
+def swap_info():
     # get the swap memory details (if exists)
     swap = psutil.swap_memory()
     print(f"Total: {get_size(swap.total)}")
     print(f"Free: {get_size(swap.free)}")
     print(f"Used: {get_size(swap.used)}")
-    print(f"Percentage: {swap.percent}%")
+    print(f"Used percentage: {swap.percent}%")
+    
+    result = [get_size(swap.total), get_size(swap.free), get_size(swap.used),  f'{get_size(swap.percent)}%']
+    
+    return result
+
+def main():
+    pass
 
 if __name__ == '__main__':
     main()
